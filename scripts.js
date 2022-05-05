a39845a549190d68bf9d800874ba2227c17ef532
Van-Uy3n <ngov778@hsd.k12.or.us>
2022-05-05 09:23:24 -0700

    creating div obj & adding event listener

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
 alert("Did you see my bag?")
+//Paragraph function
 var paragraph = document.createElement('P');
-paragraph.textContent = 'The DOM is the bomb!'
+paragraph.textContent = 'Johnny Suh is the moment!'
 document.body.appendChild(paragraph); 
+//Event listener function
+var div = document.createElement('DIV'); 
+div.style.height = '100vh';
+document.body.appendChild(div); 
+div.appendChild(paragraph); 
+div.addEventListener('mousemove', function(event) {
+    console.log(event); 
+    var x = event.clientX;
+    var y = event.clientY; 
+    div.textContent = x + ', ' + y; 
+    div.style.backgroundColor = 'rgb(' + x + ', ' + y + ', 100)'
+}); 
