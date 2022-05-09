c9d8d7924cdbb372d586f237b36308ba2a459409
Van-Uy3n <ngov778@hsd.k12.or.us>
2022-05-09 09:50:56 -0700

    relocating appendChild(paragraph)

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
-alert("Did you see my bag?")
 //Paragraph function
 var paragraph = document.createElement('P');
-paragraph.textContent = 'Johnny Suh is the moment!'
-document.body.appendChild(paragraph); 
+paragraph.textContent = 'Johnny Suh is talented, brilliant, incredible, amazing, showstopping, spectacular!'
 //Event listener function
 var div = document.createElement('DIV'); 
 div.style.height = '100vh';
+document.body.appendChild(paragraph); //Before div so para. can appear first at top 
 document.body.appendChild(div); 
-div.appendChild(paragraph); 
 div.addEventListener('mousemove', function(event) {
     console.log(event); 
     var x = event.clientX;
